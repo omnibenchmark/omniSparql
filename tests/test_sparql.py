ed334d4ed82c655db469651edf465194505ba9ad
Anthony Sonrel <anthony.sonrel@uzh.ch>
2022-06-24 15:17:45 +0200

    Add first tests.

---
+++ tests/test_sparql.py
@@ -0,0 +1,20 @@
+from omni_sparql.sparql import getSparqlQuery
+
+def test_query_from_sparql():
+    "Tests the sparql queries: 1) if they all return a string without 'input' tag."
+    
+    # set functions without def args and a dummy arg to be sued
+    no_def = {
+        "datasets_from_project": "omni_batch_processed", 
+        "random": "random" ## TODO: remove and replace with other queries
+    }
+    method_list = [method for method in dir(getSparqlQuery) if method.startswith('__') is False]
+
+    for callable in method_list: 
+        fun = getattr(getSparqlQuery, callable )
+        if callable in list(no_def.keys()): 
+            out = fun(no_def[callable])
+        else: 
+            out = fun()
+        assert type(out) == str, "Error: output is not str."
+        assert out.find("##INPUT") == -1, "Error: ##INPUT tag still here."
